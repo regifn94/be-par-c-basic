bb3004153a1df3c0e7f3ce5ee43c20324666d9cc
Regi Fernando <najoanregi@gmail.com>
2026-01-19 14:12:47 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        
+
+        int i = 5;
+        System.out.println(i);
+
+        String text = "Hello Timo";
+        System.out.println(text);
+    }
+}
