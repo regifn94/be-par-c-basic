cbda6d24d99bdf899f82e5156c14850452b073ce
Regi Fernando <najoanregi@gmail.com>
2026-02-04 14:24:58 +0800

    add looping

---
+++ Looping.java
@@ -0,0 +1,37 @@
+public class Looping {
+    public static void main(String[] args) {
+
+        whileLoop();
+
+        /**
+         *       *
+         *      ***
+         *     *****
+         *    *******
+         *   *********
+         * */
+    }
+
+    static void whileLoop(){
+        int n = 5;
+        int i =1;
+        while(i<=n){
+            int m =1;
+            int k = 5;
+            while(k>=i){
+                System.out.print(" ");
+                k--;
+            }
+            while (m<=(i*2) -1){
+                System.out.print("*");
+                m++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+
+    static void forLoop(){
+
+    }
+}
